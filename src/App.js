789ccb911f92129a09f6a6a261a7d028dc81c217
RichardEsquivel <rlesquivel@gmail.com>
2019-08-14 10:06:09 -0500

    Exported specials component from specials.js and imported into Apps.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Logo from './components/DisplayComponents/Logo';
 import Display from './components/DisplayComponents/Display.js';
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers.js';
 import Operators from './components/ButtonComponents/OperatorButtons/Operators.js';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials.js';
 
 function App() {
 	// STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
