a92a8b84827269f806a724bd882b635bf5d0fa62
RichardEsquivel <rlesquivel@gmail.com>
2019-08-14 08:01:31 -0500

    Export Display from display .js and import Display inside App.js

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+//Define component, display takes data through props
+const Display = (props) => {
+	return <div>{/* Display any props data here */}</div>;
 };
+
+//exporting display.js component so I can import and use it in other files
+export default Display;
